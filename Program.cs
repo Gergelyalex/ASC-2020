b53adf749f11c034f2afb128586d735066740c4e
Gergelyalex <72394958+Gergelyalex@users.noreply.github.com>
2020-10-16 14:03:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mooreslaw
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much greater the computational power ?");
+            int i = 1, j = 0, n;
+            n = int.Parse(Console.ReadLine());
+            while (i < n)
+            {
+                i = i * 2;
+                j = j + 2;
+
+            }
+            Console.WriteLine(j);
+            Console.ReadLine();
+        }
+    }
+}
